1d0f0c6821906029b274e8dfe1d66f6732a7d4f0
reuvensh <reuvenshirazi@gmail.com>
2024-09-04 12:56:14 +0300

    Update vuln_app.java

--- vuln_app.java
+++ vuln_app.java
@@ -1,44 +1,50 @@
-import java.io.*;
-import com.fasterxml.jackson.core; 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
-import com.caucho.config.types.ResourceRef;
-public class HttpSerializationExample {
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/serialize-deserialize", new SerializeDeserializeHandler());
-        server.start();
-        System.out.println("Server is running on port 8080...");
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
 
-    static class SerializeDeserializeHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            InputStream requestBody = exchange.getRequestBody();
-            ObjectMapper mapper = new ObjectMapper();
-            ResourceRef VAR = mapper.readValue(requestBody, ResourceRef.class);
-            String filename = "serializedJsonNode.bin";
-            serialize(VAR, filename);
-            JsonNode deserializedNode = (JsonNode) deserialize(filename);
-            exchange.sendResponseHeaders(200, 0);
-            OutputStream responseBody = exchange.getResponseBody();
-            responseBody.write(deserializedNode.toString().getBytes());
-            responseBody.close();
-        }
-    }
+public class MySQLExample {
+    public static void main(String[] args) {
+        // 1. Establish a connection to the MySQL database
+        String url = "jdbc:mysql://localhost:3306/mydatabase"; // Replace with your database URL
+        String user = "root"; // Replace with your MySQL username
+        String password = "password"; // Replace with your MySQL password
 
-    public static void serialize(Object obj, String filename) throws IOException {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
-            out.writeObject(obj);
-        }
-    }
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
 
-    public static Object deserialize(String filename) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
-            return in.readObject();
+        try {
+            // Establish the connection
+            connection = DriverManager.getConnection(url, user, password);
+
+            // 2. Create a statement object to send SQL queries to the database
+            statement = connection.createStatement();
+
+            // 3. Execute a SQL query and retrieve the result set
+            String query = "SELECT * FROM users"; // Replace with your query
+            resultSet = statement.executeQuery(query);
+
+            // Process the result set
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+
+                System.out.println("ID: " + id + ", Name: " + name + ", Email: " + email);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Clean up resources
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
